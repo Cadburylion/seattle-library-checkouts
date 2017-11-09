46f776585c4c2b8e3be0ca6a0e4d0c1a6bd2b559
Matthew Parker <cadburylion@outlook.com>
2017-11-09 15:52:21 -0800

    Added google book info navigation, icons, and fixed up grid styles

--- src/component/book-view/index.js
+++ src/component/book-view/index.js
@@ -1,22 +1,41 @@
 import React from 'react'
 import './style.scss'
+import FontAwesome from 'react-fontawesome'
 
 let BookView = (props) => {
   console.log('BookView props: ', props)
+  let {volumeInfo} = props.bookSearchResult.items[props.bookVersion]
+  let imageLinks = volumeInfo.imageLinks ? volumeInfo.imageLinks : ''
+  let title = volumeInfo.title ? volumeInfo.title : ''
+  let subtitle = volumeInfo.subtitle ? volumeInfo.subtitle : ''
+  let authors = volumeInfo.authors ? volumeInfo.authors : ''
+  let description = volumeInfo.description ? volumeInfo.description : ''
   return(
     <div className='book-view-container'>
       <div className='book-information-container'>
-        <img className='cover' src={props.bookSearchResult.items[props.bookVersion].volumeInfo.imageLinks.thumbnail} alt='book cover' />
-        <div className='book-view-close' onClick={props.bookViewToggle}>
-          <span></span>
-          <span></span>
-        </div>
-        <p className='book-title' onClick={props.bookNext}>{props.bookSearchResult.items[props.bookVersion].volumeInfo.title}</p>
-        <p className='book-subtitle'>{props.bookSearchResult.items[props.bookVersion].volumeInfo.subtitle}</p>
-        <p className='book-author' onClick={props.bookPrevious}>{props.bookSearchResult.items[props.bookVersion].volumeInfo.authors.toString()}</p>
+        <img className='cover' src={imageLinks.thumbnail} alt='book cover' />
+
+        <FontAwesome
+          onClick={props.bookViewToggle}
+          className={'close-icon cursor'}
+          name='times'
+        />
+        <FontAwesome
+          onClick={props.bookNext}
+          className={'left-arrow-icon cursor'}
+          name='arrow-left'
+        />
+        <FontAwesome
+          onClick={props.bookPrevious}
+          className={'right-arrow-icon cursor'}
+          name='arrow-right'
+        />
+        <p className='book-title'>{volumeInfo.title}</p>
+        <p className='book-subtitle'>{volumeInfo.subtitle}</p>
+        <p className='book-author'>{volumeInfo.authors}</p>
       </div>
       <div className='book-description-container'>
-        <p className='book-description'>{props.bookSearchResult.items[props.bookVersion].volumeInfo.description}</p>
+        <p className='book-description'>{volumeInfo.description}</p>
       </div>
     </div>
   )
